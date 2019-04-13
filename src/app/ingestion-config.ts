6c1faeee4531e189e2443e82e2d93a4e6efc4ed7
shaurav <you@example.com>
2019-04-13 16:41:09 +0530

    conditional disable input

--- src/app/ingestion-config.ts
+++ src/app/ingestion-config.ts
@@ -1,11 +1,13 @@
 export class IngestionConfig{
     
     constructor(
-	// public connectionName: string,
+	public connectionName: string,
     public filestoreLocation: string,
-    public templateId: string
-    // public objectType: string,
-    // public contentLess: boolean
+    public templateId: string,
+    public objectType: string,
+    public contentLess: boolean,
+    public csvIngestionEnabled:boolean,
+    public csvFileLocation:String
 
     
   ) {  }
--- src/app/ingestion/ingestion.component.ts
+++ src/app/ingestion/ingestion.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IngestionConfig } from '../ingestion-config';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-ingestion',
@@ -9,18 +10,36 @@ import { IngestionConfig } from '../ingestion-config';
 export class IngestionComponent implements OnInit {
 
   // private  connections:  Array<object> = [];
+  submitted = false;
 
-  // constructor(private  apiService:  ApiService) { }
+  constructor(private  apiService:  ApiService) { }
 
-private  ingestion = new IngestionConfig('','');
+private  ingestion = new IngestionConfig('','','','',false,false,'');
   ngOnInit() {
     // this.findConnectionByType('Target');
   }
 
-  // public  findConnectionByType(type:string){
-	// 	this.apiService.findConnectionByType(type).subscribe((data:  Array<object>) => {
-	// 		this.connections  =  data;
+  public  findConnectionByType(type:string){
+		// this.apiService.findConnectionByType(type).subscribe((data:  Array<object>) => {
+		// 	this.connections  =  data;
 			
-	// 		console.log(data);
-  //   });
+			console.log(this.ingestion);
+    // });
   }
+
+  public onSubmit() { 
+    // this.submitted = true;
+      console.log(this.ingestion);
+  }
+  public checkValue(event){
+
+    console.log(event);
+  }
+
+  isDisabled = true;
+    triggerSomeEvent() {
+        this.isDisabled = !this.isDisabled;
+        return;
+    }
+}
+
