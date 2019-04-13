ce79f61055cf128c2062ad447c61d5006ed0830a
shaurav <you@example.com>
2019-04-13 12:28:01 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IngestionComponent } from './ingestion/ingestion.component';
+import { FormtemplateComponent } from './formtemplate/formtemplate.component';
+import { NavbarTopComponent } from './navbar-top/navbar-top.component';
+import { NavbarSideComponent } from './navbar-side/navbar-side.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IngestionComponent,
+    FormtemplateComponent,
+    NavbarTopComponent,
+    NavbarSideComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ingestion/ingestion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ingestion',
+  templateUrl: './ingestion.component.html',
+  styleUrls: ['./ingestion.component.css']
+})
+export class IngestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
